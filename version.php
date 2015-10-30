adf63ab5e4db902901231dd95e6aba622e06b748
Bas Brands <bas@sonsbeekmedia.nl>
2015-10-30 10:09:50 +0100

    bumb version

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2015102901;
+$plugin->version   = 2015103000;
 $plugin->release = '2.9.0';
 $plugin->maturity = MATURITY_BETA;
 $plugin->requires  = 2015051100;
