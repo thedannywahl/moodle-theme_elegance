ea78425e8fadf6cfc606a3823446ad103945ae73
Bas Brands <bmbrands@gmail.com>
2018-06-19 16:17:13 +0200

    Update version.php

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016081500;
+$plugin->version   = 2018061900;
 $plugin->release = '3.1.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires  = 2016052300;
